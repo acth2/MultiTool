c34c9bc6ebaba48e5b8296f6ccc5d53aa2798551
acth2 <iliane57950@gmail.com>
2025-02-16 14:15:22 +0100

    Added a background and being able to compile it without losing the icons

--- src/fr/acth2/mt/components/MainWindow.java
+++ src/fr/acth2/mt/components/MainWindow.java
@@ -5,13 +5,16 @@ import fr.acth2.mt.utils.Refs;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 public class MainWindow extends JFrame {
     public MainWindow(int id) {
         super("India tech");
         setResizable(false);
 
-        Image background = new ImageIcon("icons/background.png").getImage();
+        Image background = new ImageIcon(
+                getClass().getResource("/icons/background.png")
+        ).getImage();
         JPanel panel = new JPanel(null) {
             @Override
             protected void paintComponent(Graphics g) {
@@ -22,12 +25,16 @@ public class MainWindow extends JFrame {
         setContentPane(panel);
 
 
-        Image image = getToolkit().getImage("icons/img.png");
+        Image image = new ImageIcon(
+                getClass().getResource("/icons/img.png")
+        ).getImage();
         Cursor c = getToolkit().createCustomCursor(image , new Point(getX(),
                 getY()), "img");
         setCursor(c);
 
-        setIconImage(getToolkit().getImage("icons/mts.png"));
+        setIconImage(new ImageIcon(
+                getClass().getResource("/icons/mts.png")
+        ).getImage());
 
 
         if (id == Refs.MAIN_WINDOW) {
@@ -36,12 +43,16 @@ public class MainWindow extends JFrame {
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             setVisible(true);
 
-            Image mainIcon = getToolkit().getImage("icons/logo.png");
+            Image mainIcon = new ImageIcon(
+                    getClass().getResource("/icons/logo.png")
+            ).getImage();
             JLabel mainLabel = new JLabel(new ImageIcon(mainIcon));
             mainLabel.setBounds(245, 0, 500, 250);
             add(mainLabel);
 
-            Image musicIcon = getToolkit().getImage("icons/music.png");
+            Image musicIcon = new ImageIcon(
+                    getClass().getResource("/icons/music.png")
+            ).getImage();
             JLabel musicLabel = new JLabel(new ImageIcon(musicIcon));
             musicLabel.setBounds(85, 313, 64, 64);
             add(musicLabel);
@@ -51,7 +62,9 @@ public class MainWindow extends JFrame {
             m.setBounds(43, 300, 150, 100);
             add(m);
 
-            Image downIcon = getToolkit().getImage("icons/mts-dl.png");
+            Image downIcon = new ImageIcon(
+                    getClass().getResource("/icons/mts-dl.png")
+            ).getImage();
             JLabel downLabel = new JLabel(new ImageIcon(downIcon));
             downLabel.setBounds(5, 655, 400, 100);
             add(downLabel);
